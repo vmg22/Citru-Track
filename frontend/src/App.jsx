2346610c22c361a8be9426c3622f5fd3230e3603
juan marcelo <juanmarcelo84@gmail.com>
2025-11-16 18:40:57 -0300

    componente sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./page/Dashboard/DashboardPage";
+import DashboardPrincipal from "./page/Dashboard/components/DashboardPrincipal";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DashboardPage />} />
+        <Route path="/dashboard" element={<DashboardPrincipal />} />
+
       </Routes>
     </BrowserRouter>
   );
