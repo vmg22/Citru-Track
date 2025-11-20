518dd1f5ed56a5d3900ffd0be8cdb9711db44527
ChocobarMatias <111932821+ChocobarMatias@users.noreply.github.com>
2025-11-20 00:40:12 -0300

    imagen de pestaña logo, mail de codigo pedido

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,12 +2,27 @@ const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config();
 
+//-------------local-----------
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || '127.0.0.1',
+//   port: process.env.DB_PORT || 3306,
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASS || '',
+//   database: process.env.DB_NAME || 'citrustack',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   namedPlaceholders: true
+// });
+
+
+//-------------vps-----------
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'citrustack',
+  host: process.env.DB_HOST2 || '127.0.0.1',
+  port: process.env.DB_PORT2 || 3306,
+  user: process.env.DB_USER2 || 'root',
+  password: process.env.DB_PASS2 || '',
+  database: process.env.DB_NAME2 || 'citrustack',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
