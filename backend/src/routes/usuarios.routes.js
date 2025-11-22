ca447f795a9c5301d6f4e588b7f28f7a27924d06
denisesancheez <denisesofiasanchez2@gmail.com>
2025-11-22 17:44:18 -0300

    traigo cambios

--- backend/src/routes/usuarios.routes.js
+++ backend/src/routes/usuarios.routes.js
@@ -4,7 +4,7 @@ const ctrl = require('../controllers/usuariosController');
 const auth = require('../middleware/auth');
 const { permitirRoles } = require('../middleware/roles');
 
-router.get('/', auth, permitirRoles('admin'), ctrl.list);
-router.post('/', auth, permitirRoles('admin'), ctrl.create);
+router.get('/', permitirRoles('admin'), ctrl.list);
+router.post('/', permitirRoles('admin'), ctrl.create);
 
 module.exports = router;
