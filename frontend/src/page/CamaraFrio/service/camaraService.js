677066891fd84ce751c9088a1501582b308f332f
Agustina Pucharras <pucharra81@gmail.com>
2025-11-23 15:19:00 -0300

    camaras andando

--- frontend/src/page/CamaraFrio/service/camaraService.js
+++ frontend/src/page/CamaraFrio/service/camaraService.js
@@ -7,3 +7,18 @@ export const getAllCamaras = async ()=>{
     const response = await axios.get(CAMARA_URL)
     return response.data
 }
+
+export const createCamara = async(camaraData)=>{
+    const response = await axios.post(CAMARA_URL, camaraData)
+    return response.data
+}
+
+export const editCamara = async(id_camara, camaraData)=>{
+    const response = await axios.patch(`${CAMARA_URL}/${id_camara}`, camaraData)
+    return response.data
+}
+
+export const deleteCamara = async(id_camara)=>{
+    const response = await axios.put(`${CAMARA_URL}/${id_camara}`)
+    return response.data
+}
